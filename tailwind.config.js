4336f1b2694bcf9d16b66a744085f2246b205d89
Dhruvpuri Goswami <92724441+Dhruvpuri-Goswami@users.noreply.github.com>
2023-08-31 17:28:34 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#242424",
+        "color-secondary": "#4d6ddc",
+        "color-yellow" : "#fa991a",
+      }
+    },
+  },
+  plugins: [],
+}
+
